203e61588b5683112d5c4ce3fd56b008f6ff3446
Benno <61586142+Foxydiot@users.noreply.github.com>
2023-09-27 10:04:00 +0200

    Provide basic file structure

---
+++ debugger.py
@@ -0,0 +1,6 @@
+'''
+    Debugging code for the game.
+    Written by: 
+'''
+def debug():
+    print("Seems to be working fine!")
---
+++ main.py
@@ -0,0 +1,8 @@
+''' 
+    Main code for the challenge week game.
+    Written by:
+    Class: BC11K @ Hogeschool Rotterdam
+'''
+import debugger # Imports the debugger.py file as a library.
+
+debugger.debug() # Breakdown: "debug()" calls the function inside the file "debugger".

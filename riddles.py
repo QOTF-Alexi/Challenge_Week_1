c132cc9c0e41145dc863c896aaaaad88869a6626
Ricccii <146175443+Ricccii@users.noreply.github.com>
2023-09-29 00:57:59 +0200

    start riddlegame

---
+++ riddles.py
@@ -0,0 +1,17 @@
+# player answers riddles to get words difficulty = easy
+# players then needs to decipher a word from the riddle answers
+# the deciphered word is needed to unlock a room with an item [Voice message device] 
+
+def riddlegame(): 
+
+    riddles = ("If you are running in a race and you pass the person in second place, what place are you in?",
+               "You walk into a room that contains a match, a kerosene lamp, a candle and a fireplace. What would you light first?",
+               " I am an odd number. Take away one letter and I become even. What number am I?",
+               " It belongs to you, but other people use it more than you do. What is it?"
+               )
+    
+    answers = ("Second place", "A match", "Seven", "Name")
+
+    decipher_word = "SAVE" 
+
+    
--- wordgame.py
+++
@@ -1,4 +0,0 @@
-# player guesses series of words  (charades type of word guessing game) difficulty = easy
-# players then needs to decipher another word from the words theyve guessed
-# the deciphered word is needed to unlock a room with an item and access to the next location 
-

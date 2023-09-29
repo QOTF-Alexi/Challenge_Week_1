35a11623ae220291fe23dcbe2391a45290ab411b
Benno <61586142+Foxydiot@users.noreply.github.com>
2023-09-29 12:33:21 +0200

    change up routing activity

--- mathLol.py
+++ mathLol.py
@@ -1,12 +1,11 @@
 '''
-    The biggest waste of time ever.
-    This code has no consequences for game progression.
+    You weren't expecting this one, were you?
     Written by: Benno
 '''
 
 from random import randint
 
-def calcPi():
+def easy():
     pi = 3.14159
 
     print("You have to calculate the circumference of a circle!")
@@ -18,47 +17,24 @@ def calcPi():
         print("Oh. Shouldn't everyone know that?")
         return "lost"
 
-def addRandom():
-    num1 = randint(0, 50)
-    num2 = randint(0, 50)
-    print(f"Please calculate {num1}+{num2}")
-    num3 = int(input("Please? "))
-    if num3 == num1+num2:
-        print("That seems accurate enough")
-    else:
-        print("So close yet so far")
+def medium():
+    print("One math question coming right up!")
 
-def subRandom():
-    num1 = randint(0, 50)
-    num2 = randint(0, 50)
-    print(f"Please calculate {num1}-{num2}")
-    num3 = int(input("Please? "))
-    if num3 == num1-num2:
-        print("That seems accurate enough")
-    else:
-        print("So close yet so far")
-
-def thisDoesNothing():
-    playWhich = randint(0, 2)
-    if playWhich == 0:
-        calcPi()
-    elif playWhich == 1:
-        addRandom()
-    elif playWhich == 2:
-        subRandom()
+def hard():
+    print("One hard question")
 
 def debugTest():
     print("Enter the correct one here!")
-    calcPi()
+    easy()
     print("Enter the incorrect one here!")
-    calcPi()
+    easy()
 
     print("Enter the correct one here!")
-    addRandom()
+    medium()
     print("Enter the incorrect one here!")
-    addRandom()
+    medium()
 
     print("Enter the correct one here!")
-    subRandom()
+    hard()
     print("Enter the incorrect one here!")
-    subRandom()
+    hard()

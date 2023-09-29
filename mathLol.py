0c36934bb68abf229b186b842cecff61f46af118
Benno <61586142+Foxydiot@users.noreply.github.com>
2023-09-29 12:46:05 +0200

    overhaul activity

--- mathLol.py
+++ mathLol.py
@@ -1,11 +1,12 @@
 '''
-    You weren't expecting this one, were you?
+    The biggest waste of time ever.
+    This code has no consequences for game progression.
     Written by: Benno
 '''
 
 from random import randint
 
-def easy():
+def medium():
     pi = 3.14159
 
     print("You have to calculate the circumference of a circle!")
@@ -17,11 +18,28 @@ def easy():
         print("Oh. Shouldn't everyone know that?")
         return "lost"
 
-def medium():
-    print("One math question coming right up!")
+
+def easy():
+    num1 = randint(0, 50)
+    num2 = randint(0, 50)
+    print(f"Please calculate {num1}+{num2}")
+    num3 = int(input("Please? "))
+    if num3 == num1+num2:
+        print("That seems accurate enough")
+    else:
+        print("So close yet so far")
+        return "lost"
 
 def hard():
-    print("One hard question")
+    print("Please solve 2^3+2^0+8/4")
+    answer = input("Please? ")
+    if answer == (2^3+2^0+8/4):
+        print("That is indeed correct")
+    else:
+        print("Nope.")
+        return "lost"
+
+
 
 def debugTest():
     print("Enter the correct one here!")

50770743cf1a73a140db167f0a617c71c46d8600
Ricccii <146175443+Ricccii@users.noreply.github.com>
2023-10-02 16:07:51 +0200

    finishin touched done

--- quiz.py
+++ quiz.py
@@ -11,39 +11,77 @@ from time import sleep
 
 def quizdragon(name):
 
-    miscStuffLib.lines(117)
+    print()
+    print("You entered the den and immediately feel chills in your spine. The den is filled with bones and suddenly smell something funny.")
+    sleep(1)
+    print("Then you realize that you can't smell in space and you're just smelling yourself. lol ")
+    sleep(1)
+    print("After looking around the den you come across a big door and decide to enter it. Then you suddenly hear a loud voice.")
+    sleep(1)
+    print("You suddenly see something appear from the shadows in front of you.")
+    sleep(2)
+
+    print(r"""
+
+                                    _/|__
+                _,-------,        _/ -|  \_     /~>.
+             _-~ __--~~/\ |      (  \   /  )   | / |
+          _-~__--    //   \\      \ *   * /   / | ||
+       _-~_--       //     ||      \     /   | /  /|
+      ~ ~~~~-_     //       \\     |( " )|  / | || /
+          \   //         ||    | VWV | | /  ///
+        |\     | //           \\ _/      |/ | ./ |
+        | |    |// __         _-~         \// |  /
+       /  /   //_-~  ~~--_ _-~  /          |\// /
+      |  |   /-~        _-~    (     /   |/ / /
+     /   /           _-~  __    |   |____|/
+    |   |__         / _-~  ~-_  (_______  `\
+    |      ~~--__--~ /  _     \        __\)))
+     \               _-~       |     ./  \
+      ~~--__        /         /    _/     |
+            ~~--___/       _-_____/      /
+             _____/     _-_____/      _-~
+          /^<  ___       -____         -____
+             ~~   ~~--__      ``\--__       ``\
+                        ~~--\)\)\)   ~~--\)\)\) 
+
+    """)
+    miscStuffLib.lines(117) 
     print("| Thou who is courageous enough to enter Veldora's Den shall be given the chance to recieve thy blessing.           |")
-    sleep(1)
+    sleep(0.75)
     print("| Welcome young traveller! I am the Star Dragon Veldora of planet Eris.                                             |")
-    sleep(1)
+    sleep(0.75)
     print("| You've woken me from my deep slumber and choose to enter my den.                                                  |")
-    sleep(1)
+    sleep(0.75)
     print("| You will be asked a series of questions and your correct answers will determine if you are worthy of my blessing. |")
-    sleep(1)
+    sleep(0.75)
     print("| Good luck!                                                                                                        |")
-    sleep(1)
+    sleep(0.75)
     
     miscStuffLib.lines(117)
 
     start = input("Type \"START\" to start the quiz: ")
-    if start == "start":
+    if start.lower(  ) == "start":
 
         questions = ("What is the name of the dragon that is bestowing you one of its heavenly gifts?",
                     "Which ocean is the biggest ocean in the world?",
                     "What is the name of the planet you are saving?",
                     "Which continent is fully surrounded by a body of ocean?",
                     "What is the capital city of Canada?",
-                    "What is the name of the pearcoach of class K?" )
+                    "What is the name of the pearcoach of class K?",
+                    "What programming language do we need to learn the next semester?"
+                      )
         
         choices = (("A. Baldora","B. Veldora ","C. Valdrora", "D. Beldrora"),
                 ("A. Pacific Ocean","B. Atlantic Ocean","C. Indian Ocean", "D. Artic Ocean"),
                 ("A. Airis","B. Esis","C.Arsis", "D. Eris"),
                 ("A. Antartica","B. South America","C. Africa", "D. Australia/Oceania"),
                 ("A. Vancouver","B. Ottawa","C. Toronto", "D. Québec"),
-                ("A. Cornelly","B. Cornelius","C. Cornelis", "D. Cornelion")
+                ("A. Cornelly","B. Cornelius","C. Cornelis", "D. Cornelion"),
+                ("A. C#, B. Java, C. C++, D. CSS")
                 )
         
-        right_answers = ("B","A","D","D","B","C")
+        right_answers = ("B","A","D","D","B","C","A")
         
         numbering = 0
         points = 0
@@ -68,7 +106,7 @@ def quizdragon(name):
         miscStuffLib.lines(77)
 
         result = int(points)
-        print("| Your total score is ", result, "/6", " "*51, "|", sep="")
+        print("| Your total score is ", result, "/7", " "*51, "|", sep="")
         sleep(1)
         print("| I shall now determine if you are worthy of the gift based on your points. |")
 
@@ -77,7 +115,7 @@ def quizdragon(name):
         print()
         print()
 
-        if result > 4: #gives the player an item 
+        if result > 6: #gives the player an item 
 
             miscStuffLib.lines((112+len(name)))
 
@@ -85,7 +123,7 @@ def quizdragon(name):
             sleep(1)
             print("| Although you can not use it to its full potential, you will still be able to travel the planet more easier.", " "*(2+len(name)), "|", sep="")
             sleep(1)
-            print("| You are able to use this gift to skip a obstacle once in your journey ahead.", " "*(33+len(name)), "|", sep="")
+            print("| You are able to use this gift to skip an obstacle ONCE in your journey ahead.", " "*(33+len(name)), "|", sep="")
             sleep(1)
             print("| Best of luck and have a safe journey.", " "*(72+len(name)), "|", sep="")
         
--- riddles.py
+++ riddles.py
@@ -6,6 +6,7 @@ import miscStuffLib
 from time import sleep
 
 def riddle_game():
+
     def ask_riddle(riddle, right_ans):
         attempts = 0
 
@@ -26,6 +27,29 @@ def riddle_game():
         print("You've used up all your attempts. The correct answer is: {}".format(right_ans))
         return False
 
+    print()
+    print("After realizing what kind of situation you are in, you decided to just deal with it and take look around the rocket ship.")
+    sleep(0.75)
+    print("While looking around you just see a bunch of normal things you would see in a rocket ship.")
+    sleep(0.75)
+    print("Finally you found the exit but it's locked. Suddenly a pop-up window appears in from of your face. ")
+    sleep(0.75)
+    print("In the window it says: ")
+    sleep(0.75)
+    print()
+    miscStuffLib.lines(100)
+    print("|Congratulations on finding the exit! To open the door you would need to enter a password.         |")
+    sleep(0.75)
+    print("|But first you would need to play a game to get hints on what the password is.                     |")
+    sleep(0.75)
+    print("|The game is simple. You just need to answer a bunch of riddles. You have 3 attemps on each riddle.|")
+    sleep(0.75)
+    print("|Remember each correct answer. You're gonna need it for the password.                              |")
+    sleep(0.75)
+    print("|Good luck!                                                                                        |")
+    sleep(0.75)
+    miscStuffLib.lines(100)
+
     riddles = {
         "What is a five-letter word that becomes shorter when you add two letters to it?": "short",
         "What goes up but never goes down?": "age",
@@ -33,34 +57,94 @@ def riddle_game():
         "You see me once in June, twice in November and not at all in May. What am I?": "e"
     }
 
-    for riddle, right_ans in riddles.items():
-        ask_riddle(riddle, right_ans)
+    ready_input = ""
+    while ready_input.upper() != "READY":
+        ready_input = input("To start the game type \"READY\".")
+        if ready_input.upper() == "READY":
+            for riddle, right_ans in riddles.items():
+                ask_riddle(riddle, right_ans)
+        else:
+            print("That was not correct!")
+
+    
 
     print()
     print()
-    miscStuffLib.lines(90)
+    miscStuffLib.lines(97)
     print("|                                                                                               |")
-    print("|Congratulations on completing the riddles! The correct answers were: Short, Age, Vonneke and E.|")
+    sleep(1)
+    print("|Congratulations on completing the riddles!                                                     |")
+    sleep(1) 
+    print("|The correct answers were: Short, Age, Vonneke and E.                                           |")
+    sleep(1)
     print("|Remember these answers well because you're going to need it to help you unlock the exit.       |")
-    print("|Here is the scroll to help you decipher the code for the exit.                                 |")
+    sleep(1)
+    print("|Here is a projector device to help you decipher the code for the exit.                         |")
+    sleep(1)
     print("|                                                                                               |")
-    miscStuffLib.lines(90)
+    sleep(1)
+    miscStuffLib.lines(97)
+    sleep(3)
     return "projector device"
-
+    
 riddle_game()
-
 #pt 2 of the game 
 # with all the answers from the riddle they would need to form a word 
 #it'll work as a passcode to unlock the door to go outside the rocket 
 
 def doorcode():
+    miscStuffLib.clear()
+    print()
+    print()
+    miscStuffLib.lines(73)
+    print("|Congratulations on solving the riddles.                                |")
+    sleep(0.75)
+    print("|To finally unlock the door you would need to open the projector device.|")
+    sleep(0.75)
+    print("|Type \"OPEN\" to see the message in the device.                          |")
+    miscStuffLib.lines(73)
+    open_device = input()
+    print()
+
+    if open_device.upper() == "OPEN":
+        print("The device suddenly opened and a screen popped out of it. ")
+        sleep(1)
+        print("In the screen it says: ")
+        sleep(1)
+        miscStuffLib.lines(102)
+        print("|This is a message for the people who are trying to open the door.                                   |")
+        sleep(1)
+        print("|If i am correct the password for the door consists out of two words.                                |")
+        sleep(1)
+        print("|The first word you will need decipher from the words you got from the riddle game.                  |")
+        sleep(1)
+        print("|You will need to put the answers under each other in the same order on how the questions were asked.|")
+        sleep(1)
+        print("|By then you would need to see if theres a pattern in those stack of words.                          |")
+        sleep(1)
+        print("|If you figured out the word you should put the name of the planet you're in after it.               |")
+        sleep(1)
+        print("|That's pretty much everything i can say, i hope it helps.                                           |")
+        sleep(1)
+        miscStuffLib.lines(102)
+    
+    print()
+    print()
+    
+    print("The device suddenly closed. You go to the door and clicked on the tablet to enter the password.")
+    sleep(0.5)
+    print("On the screen you see \"_ _ _ _   _ _ _ _ \"")
+    sleep(0.5)
+
     password = "SAVE ERIS"
 
     unlock_door = input("Enter the password to open the door: ")
 
     if unlock_door.upper() == password:
-        print("Door unlocked. Welcome to planet Eris! May your adventures begin.")
+        print()
+        print("Door unlocked. Congratulations! Welcome to planet Eris! May your adventures begin.")
     else:
-        print("Incorrect passcode. The door remains locked. Please try again")
+        print()
+        print("Incorrect passcode. The door remains locked. Please try again.")
 
-#doorcode()
+doorcode()

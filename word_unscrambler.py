ba5ef9a88bd7ca18cbdcc5b652850a14284f8df3
norailly <146177029+Norailly@users.noreply.github.com>
2023-10-02 13:10:37 +0200

    update words

--- word_unscrambler.py
+++ word_unscrambler.py
@@ -1,4 +1,5 @@
 '''
+
 Game that lets player unscramble words
 written by: No'railly
 
@@ -7,43 +8,50 @@ import miscStuffLib
 import random
 
 def word_unscrambler():
-    valid_words = ["windows", "basecamp", "laptop", "challenge", "backpack", "mouse", "python"]
+    print("|Hello young traveller. Welcome to The letter Crater.|")
+    print("|You fell in here and got TRAPPED! To get out you need to play a game.|")
+    print("|In this game you need to unscramble all the words that are given to you.|")
+    print("|Only when you scramble 6 words correctcly, you will be teleported out of here.|")
+    print("|If not you will be stuck here forever! Good luck|")
+
+    miscStuffLib.lines(70)
+    
+    valid_words = ["windows", "basecamp", "laptop", "challenge", "backpack", "python"]
 
     guess_count = 0
 
     correct_guesses = 0
 
-    for _ in range(6):
+    for i in range(6):
         chosen_word = random.choice(valid_words)
         valid_words.remove(chosen_word)
 
-        word_as_list = list(chosen_word)
+        word_as_list = list(chosen_word) 
         random.shuffle(word_as_list)
         scrambled_word = ''.join(word_as_list)
 
-        miscStuffLib.lines(60)
+        miscStuffLib.lines(70)
+
         print("Round " + str(guess_count + 1) + ": Unscramble the word: " + scrambled_word)
         
         while True:
             user_guess = input("Enter a valid English word: ").lower()
 
             if user_guess == chosen_word:
-                print()
+                miscStuffLib.lines(70)
                 print("Correct! You unscrambled the word.")
                 correct_guesses += 1
                 break
             else:
-                print()
+                miscStuffLib.lines(70)
                 print("Try again. It's not the same as the scrambled word.")
 
         guess_count += 1
 
     if correct_guesses == 6:
+        miscStuffLib.lines(70)
         print("You have unscrambled all the words correctly. Good job!")
-        return "voice recorder"
     else:
-        print("You have completed the game. You unscrambled", correct_guesses, "words correctly.")
+        miscStuffLib.lines(70)
+        print("You have made too many mistakes. You only unscrambled", correct_guesses, "words correctly. Because of that you will stay here forever!")
         return "lost"
-
-
-word_unscrambler()
